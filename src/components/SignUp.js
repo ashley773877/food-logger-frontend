69040e17dd6b54bae83b30321223f4fa92012bd8
ashley773877 <mirandaashley16@gmail.com>
2024-02-10 23:47:33 -0500

    rendered signup form into sign in page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,12 +1,34 @@
 // page for signup form will be rendered into sign in folder
 
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+
 function SignUpForm() {
+    
     return (
         <div>
-            <h2> Sign up </h2>
-           
-            
-        </div>
+            <h3> signnnn</h3>
+        <Form>
+      <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
+            Username
+          </Form.Label>
+          <InputGroup className="mb-2">
+            <InputGroup.Text>@</InputGroup.Text>
+            <Form.Control id="inlineFormInputGroup" placeholder="Username" />
+          </InputGroup>
+        
+        
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <button type="submit">Sign In</button>
+      </Form>
+      </div>
     );
 }
 
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,13 +3,16 @@
 // have a components page for the sign up and render it here 
 
 import Form from 'react-bootstrap/Form';
+import SignUpForm from '../components/SignUp';
 
 function LoginPage() {
     return (
+        <div>
+            <SignUpForm />
         <Form>
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control type="email" placeholder="email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
@@ -17,6 +20,7 @@ function LoginPage() {
         </Form.Group>
         <button type="submit">Sign In</button>
       </Form>
+      </div>
     );
 }
 
