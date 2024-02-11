1ec1731609827b307573a16a881cd9522eb6fa88
ashley773877 <mirandaashley16@gmail.com>
2024-02-10 23:32:51 -0500

    got sign in form from boostrap and added button

---
+++ src/components/SignUp.js
@@ -0,0 +1,14 @@
+// page for signup form will be rendered into sign in folder
+
+function SignUpForm() {
+    return (
+        <div>
+            <h2> Sign up </h2>
+           
+            
+        </div>
+    );
+}
+
+
+export default SignUpForm
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,15 +2,24 @@
 // put a form in here to sign in or sign up 
 // have a components page for the sign up and render it here 
 
+import Form from 'react-bootstrap/Form';
+
 function LoginPage() {
     return (
-        <div>
-            <h2> Sign In/ Sign Up</h2>
-           
-            
-        </div>
+        <Form>
+        <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <button type="submit">Sign In</button>
+      </Form>
     );
 }
 
 
 export default LoginPage
+    
