91545985cc19b8a39ee647da44faf2d185816ee7
ashley773877 <mirandaashley16@gmail.com>
2024-02-11 00:41:35 -0500

    trying to take sign in and sign up off the same page

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,13 +2,76 @@
 
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { useRef, useState } from "react";
 
 function SignUpForm() {
+    const emailInputRef = useRef(null);
+    const passwordInputRef = useRef(null);
+   
+   
+   
+   
+   
+    const handleSignUp = (e) => {
+        e.preventDefault();
+        console.log(emailInputRef.current.value);
+        console.log(passwordInputRef.current.value);
+    
+        if (emailInputRef.current.value === "") {
+          emailInputRef.current.focus();
+          return;
+        }
+        if (passwordInputRef.current.value === "") {
+          passwordInputRef.current.focus();
+          return;
+        }
+        // the request here will be for the signup
+      };
+    
+    
+    
+    
+    
+    
+    
+    
     
     return (
         <div>
-            <h3> signnnn</h3>
-        <Form>
+            <h3> Sign Up </h3>
+
+            <fieldset>
+        <Form.Group as={Row} className="mb-3">
+          <Form.Label as="legend" column sm={2}>
+            Gender
+          </Form.Label>
+          <Col sm={10}>
+            <Form.Check
+              type="Gender"
+              label="Female"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="Gender"
+              label="Male"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios2"
+            />
+            <Form.Check
+              type="Gender"
+              label="Rather not say"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios3"
+            />
+          </Col>
+        </Form.Group>
+      </fieldset>
+        
+        <Form onSubmit={handleSignUp}>
+     
       <Form.Label htmlFor="inlineFormInputGroup" visuallyHidden>
             Username
           </Form.Label>
@@ -17,8 +80,12 @@ function SignUpForm() {
             <Form.Control id="inlineFormInputGroup" placeholder="Username" />
           </InputGroup>
         
-        
+          
         <Form.Group className="mb-3" controlId="formGroupEmail">
+          <Form.Label>age</Form.Label>
+          <Form.Control type="age" placeholder="age" />
+        </Form.Group>
+         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="email" />
         </Form.Group>
@@ -26,8 +93,10 @@ function SignUpForm() {
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <button type="submit">Sign In</button>
+        <button type="submit">Sign Up</button>
+      
       </Form>
+     
       </div>
     );
 }
--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -4,12 +4,46 @@
 
 import Form from 'react-bootstrap/Form';
 import SignUpForm from '../components/SignUp';
+import { useRef, useState } from "react";
 
 function LoginPage() {
+    const emailInputRef = useRef(null);
+    const passwordInputRef = useRef(null);
+    const [showSignIn, setShowSignIn] = useState(false);
+    
+    
+    
+    
+    
+    
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        console.log(emailInputRef.current.value);
+        console.log(passwordInputRef.current.value);
+    
+        if (emailInputRef.current.value === "") {
+          emailInputRef.current.focus();
+          return;
+        }
+        if (passwordInputRef.current.value === "") {
+          passwordInputRef.current.focus();
+          return;
+        }
+    
+    }
+    
+    
+    
+    
+    
+    
+    
     return (
+     <main>
         <div>
-            <SignUpForm />
-        <Form>
+         
+        <Form onSubmit={handleSignIn}>
+     
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" placeholder="email" />
@@ -19,8 +53,25 @@ function LoginPage() {
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
         <button type="submit">Sign In</button>
-      </Form>
+        </Form>
+        <span>
+            Don't have an account?{" "}
+            <button onClick={() => setShowSignIn(!showSignIn)}>Sign Up</button>
+          </span>
+
       </div>
+         
+          <div> 
+      <SignUpForm />
+      <span>
+            Already have an account?{" "}
+            <button onClick={() => setShowSignIn(!showSignIn)}>Sign In</button>
+          </span>
+      </div>
+
+      </main>
+    
+      
     );
 }
 
