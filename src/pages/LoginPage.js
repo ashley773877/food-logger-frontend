77a5d1ac912f314ac77bbb57d0a0e48535c0148e
ashley773877 <mirandaashley16@gmail.com>
2024-02-11 01:03:04 -0500

    put sign and sign up on different forms

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 
 // put a form in here to sign in or sign up 
 // have a components page for the sign up and render it here 
+import {Link} from 'react-router-dom'
 
 import Form from 'react-bootstrap/Form';
 import SignUpForm from '../components/SignUp';
@@ -10,71 +11,54 @@ function LoginPage() {
     const emailInputRef = useRef(null);
     const passwordInputRef = useRef(null);
     const [showSignIn, setShowSignIn] = useState(false);
-    
-    
-    
-    
-    
-    
+  
     const handleSignIn = (e) => {
-        e.preventDefault();
-        console.log(emailInputRef.current.value);
-        console.log(passwordInputRef.current.value);
-    
-        if (emailInputRef.current.value === "") {
-          emailInputRef.current.focus();
-          return;
-        }
-        if (passwordInputRef.current.value === "") {
-          passwordInputRef.current.focus();
-          return;
-        }
-    
-    }
-    
-    
-    
-    
-    
-    
-    
+      e.preventDefault();
+      console.log(emailInputRef.current.value);
+      console.log(passwordInputRef.current.value);
+  
+      if (emailInputRef.current.value === "") {
+        emailInputRef.current.focus();
+        return;
+      }
+      if (passwordInputRef.current.value === "") {
+        passwordInputRef.current.focus();
+        return;
+      }
+    };
+  
     return (
-     <main>
-        <div>
-         
-        <Form onSubmit={handleSignIn}>
-     
-        <Form.Group className="mb-3" controlId="formGroupEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <button type="submit">Sign In</button>
-        </Form>
-        <span>
-            Don't have an account?{" "}
-            <button onClick={() => setShowSignIn(!showSignIn)}>Sign Up</button>
-          </span>
-
-      </div>
-         
-          <div> 
-      <SignUpForm />
-      <span>
-            Already have an account?{" "}
-            <button onClick={() => setShowSignIn(!showSignIn)}>Sign In</button>
-          </span>
-      </div>
-
+      <main>
+        {showSignIn ? (
+          <div>
+            <Form onSubmit={handleSignIn}>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="email" />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+              <button type="submit">Sign In</button>
+            </Form>
+            <span>
+              Don't have an account?{" "}
+              <span onClick={() => setShowSignIn(!showSignIn)}>Sign Up</span>
+            </span>
+          </div>
+        ) : (
+          <div>
+            <SignUpForm />
+            <span>
+              Already have an account?{" "}
+              <span onClick={() => setShowSignIn(!showSignIn)}>Sign In</span>
+            </span>
+          </div>
+        )}
       </main>
-    
-      
     );
-}
-
-
-export default LoginPage
-    
+  }
+  
+  export default LoginPage;
+  
